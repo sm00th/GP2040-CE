2b3a3c5dd5a22dba9d0ba76ddb8461e821e6c234
Artem Savkov <artem.savkov@gmail.com>
2025-07-21 19:40:59 +0200

    initial i2c stuff, untested

---
+++ headers/interfaces/i2c/ps5_auth/ps5_auth.h
@@ -0,0 +1,44 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2025 OpenStickCommunity (gp2040-ce.info)
+ */
+
+#ifndef _PS5_AUTH_H_
+#define _PS5_AUTH_H_
+
+#include "i2cdevicebase.h"
+
+enum PS5_AUTH_CMD {
+    PS5_AUTH_CMD_WRITE = 0x00,
+    PS5_AUTH_CMD_READ = 0x83,
+};
+
+enum PS5_AUTH_ADDR {
+    PS5_AUTH_ADDR_UNK1 = 0x60,
+    PS5_AUTH_ADDR_UNK2 = 0x70,
+    PS5_AUTH_ADDR_UNK3 = 0x78,
+    PS5_AUTH_ADDR_CHALLENGE = 0x88,
+    PS5_AUTH_ADDR_SIGN = 0xa0,
+    PS5_AUTH_ADDR_RESPONSE = 0xa8,
+};
+
+class PS5Auth : public I2CDeviceBase {
+    public:
+        PS5Auth(PeripheralI2C *i2c, uint8_t addr);
+        std::vector<uint8_t> getDeviceAddresses() const override {
+            return {0x1a};
+        }
+
+        int16_t get_signature(const uint8_t *data, uint8_t *sig);
+        int16_t process_challenge(const uint8_t *challenge, uint8_t *response);
+
+    protected:
+        int16_t reset_read(uint8_t addr, uint8_t size, uint8_t *buf);
+        int16_t read(uint8_t addr, uint8_t size, uint8_t *buf);
+        int16_t write(uint8_t addr, uint8_t size, const uint8_t *data);
+
+        PeripheralI2C *m_i2c;
+        uint8_t m_address;
+};
+
+#endif /* _PS5_AUTH_H_ */
---
+++ src/interfaces/i2c/ps5_auth/ps5_auth.cpp
@@ -0,0 +1,57 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2025 OpenStickCommunity (gp2040-ce.info)
+ */
+
+#include "ps5_auth/ps5_auth.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define INIT_DATA_SIZE 3
+
+PS5Auth::PS5Auth(PeripheralI2C *i2c, uint8_t addr) : m_i2c {i2c}, m_address {addr} {
+    uint8_t unk1[32] {};
+    uint8_t unk3[4] {};
+    uint8_t unk2[] { 0x23, 0x45, 0x67, 0x89, 0xaa, 0xbb, 0xcc, 0xdd };
+
+    reset_read(PS5_AUTH_ADDR_UNK3, 4, unk3);
+    reset_read(PS5_AUTH_ADDR_UNK1, 32, unk1);
+    write(PS5_AUTH_ADDR_UNK2, 8, unk2);
+    write(0x00, 0, nullptr);
+};
+
+int16_t PS5Auth::reset_read(uint8_t addr, uint8_t size, uint8_t *buf) {
+    write(addr, 0, nullptr);
+    return read(addr, size, buf);
+}
+
+int16_t PS5Auth::read(uint8_t addr, uint8_t size, uint8_t *buf) {
+    uint8_t sendbuf[3] = { PS5_AUTH_CMD_READ, addr, size };
+    m_i2c->write(m_address, sendbuf, 3);
+    return m_i2c->read(m_address, buf, size);
+}
+
+int16_t PS5Auth::write(uint8_t addr, uint8_t size, const uint8_t *buf) {
+    uint8_t *sendbuf = (uint8_t*)malloc(size + 3);
+    sendbuf[0] = PS5_AUTH_CMD_WRITE;
+    sendbuf[1] = addr;
+    sendbuf[2] = size;
+    if (size > 0) {
+        memcpy(sendbuf + 3, buf, size);
+    }
+    return m_i2c->write(m_address, sendbuf, size + 3);
+}
+
+int16_t PS5Auth::get_signature(const uint8_t *data, uint8_t *sig) {
+    int16_t res;
+    uint8_t buf[12];
+    write(PS5_AUTH_ADDR_SIGN, 12, data);
+    res = m_i2c->read(m_address, buf, 12);
+    memcpy(sig, buf + 4, 8);
+    return res;
+}
+
+int16_t PS5Auth::process_challenge(const uint8_t *challenge, uint8_t *response) {
+    write(PS5_AUTH_ADDR_CHALLENGE, 180, challenge);
+    return reset_read(PS5_AUTH_ADDR_RESPONSE, 192, response);
+}
